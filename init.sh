97b28a521e94c6030b41b736794e0ae632da8673
Oleg Savelev <sava631@gmail.com>
2016-05-12 15:14:12 +1000

    nginx!

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#sudo ln -s ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart

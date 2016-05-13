27554799a5b36b42528ca538e5c0705a3f117223
Oleg Savelev <sava631@gmail.com>
2016-05-13 22:26:49 +1000

    gunicorn

--- etc/hello.py
+++ etc/hello.py
@@ -1,12 +1,12 @@
 CONFIG = {
     'mode': 'wsgi',
-    'working_dir': '/home/box/web/',
-    'python3': '/usr/bin/python3',
+    'working_dir': '/home/box/web',
+    'python': '/usr/bin/python',
     'args': (
         '--bind=0.0.0.0:8080',
         '--workers=16',
         '--timeout=60',
-        #'app.module',
-        'hello:app',
+		'--log-level=debug',
+        'hello',
     ),
 }
--- hello.py
+++ hello.py
@@ -1,16 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
+from cgi import parse_qs                                                        
 
-from cgi import parse_qs
+def application(environ, start_response):
 
-def app(environ, start_response):
-    data = b'Hello, World!\n'
-    start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(data)))
-    ])
-    qs =  parse_qs(environ['QUERY_STRING'])
+	queryString = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+	body = ''
+	print(queryString)
+	for key, value in queryString.items() :
+		for element in value :
+			body += key + '='                                       
+			body += element + '\r\n'
 
-    return iter([data])
+	print(body)                                                             
+		
+	status = '200 OK'
 
+	response_headers = [
+		('Content-Type', 'text/plain'),
+	]
+	start_response(status,response_headers)                                    
+
+	return [body]

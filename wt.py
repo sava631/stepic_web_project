de262fd27cd8d3871ce7616ed6f023c6a8097ba8
Oleg Savelev <sava631@gmail.com>
2016-05-15 22:01:49 +1000

    wt

--- wt.py
+++ wt.py
@@ -1,15 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import random
 
-def wt():
+def app(environ, start_response):
+    data = ''
     x = random.randint(0,4)
     y = random.randint(0,1)
     z = random.randint(0,1)
     nac = ['США', 'Германия', 'СССР', 'Великобритания', 'Япония']
     teh = ['Танки', 'Самолёты']
     tip = [' (Аркада)', ' (Реалистичные)']
-    if a != 4:
-        otvet = nac[x] + ' - ' + teh[y]
+    data = nac[x]
+    if x != 4:
+        data = nac[x] + ' - ' + teh[y]
     else:
-        otvet = nac[x] + ' - Самолёты'
-    otvet += tip[z]
-    return(otvet)
+        data = nac[x] + ' - Самолёты'
+    if y == 0:
+        data += tip[z]
+    status = '200 OK'
+    response_headers = [
+        ('Content-type','text/plain;charset=utf-8'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return [data]
